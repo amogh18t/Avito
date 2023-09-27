25c0a51f9e48cfee9a4562b1b8ac273a668112c0
amogh18t <amogh18t@gmail.com>
2023-09-27 17:08:48 +0530

    start

---
+++ src/component/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header=()=>{
+    return (
+        <div>
+            <div className="header">
+                <ul className="header-link">
+                    <li>Your</li>
+                    <li>All</li>
+                    <li>Blocked</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
