82131bfdab396078ae75f6888d6c40febfda1fc9
amogh18t <amogh18t@gmail.com>
2023-09-27 18:52:11 +0530

    header and filter

---
+++ src/component/Filter.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Filter = () => {
+    return (
+        <div className="filter">
+            <div className="Logo">
+                <p></p>
+                <span></span>
+            </div>
+            <div className="filter-button">
+                <button className="filter-button-desing">
+                    <div className="filter-logo">
+                        <span id="one"></span>
+                        <span id="two"></span>
+                        <span id="three"></span>
+                    </div>
+                    <div>
+                        Filter
+                    </div>
+                </button>
+            </div>
+        </div>
+    )
+}
+export default Filter;
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 const Header=()=>{
     return (
         <div>
-            <div className="header">
+            <div className="header">  
                 <ul className="header-link">
-                    <li>Your</li>
-                    <li>All</li>
-                    <li>Blocked</li>
+                    <Link to={"#yours"}>
+                        <button>
+                            <li id="yours">Your</li>
+                        </button>
+                    </Link>
+                    <Link to={"#all"}>
+                        <button>
+                            <li id="all">All</li>
+                        </button>
+                    </Link>
+                    <Link to={"#blocked"}>
+                        <button>
+                            <li id="blocked">Blocked</li>
+                        </button>
+                    </Link>    
                 </ul>
             </div>
         </div>
